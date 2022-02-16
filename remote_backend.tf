a7128128dde3e81490ec2e390f7cb9c19cb223c8
root <wasp6790@gmail.com>
2022-02-16 05:44:35 +0000

    added remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,13 +1,9 @@
-# If you are in a workshop...
-# Do not delete this file!
-# It's required to complete the Instruqt labs.
-
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "YIJIYEON-training"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-aws"
     }
   }
 }
